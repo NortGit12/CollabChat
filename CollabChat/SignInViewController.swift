fe2f0a2fb79c654c69e64dbb2e26aa8b09c37b2b
Jeff Norton <jeff12ntn@gmail.com>
2016-12-22 16:57:58 -0700

    Initial SignIn layout and view controller done

---
+++ CollabChat/SignInViewController.swift
@@ -0,0 +1,61 @@
+//
+//  SignInViewController.swift
+//  CollabChat
+//
+//  Created by Jeff Norton on 12/22/16.
+//  Copyright © 2016 JeffCryst. All rights reserved.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+    
+    //==================================================
+    // MARK: - _Properties
+    //==================================================
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var signInButton: UIButton!
+    
+    //==================================================
+    // MARK: - Methods
+    //==================================================
+    
+    func setupViewElements() {
+        
+        emailTextField.layer.cornerRadius = 20
+        
+        passwordTextField.layer.cornerRadius = 20
+        
+        signInButton.layer.cornerRadius = 20
+    }
+    
+    //==================================================
+    // MARK: - View Lifecycle
+    //==================================================
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupViewElements()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
